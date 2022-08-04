4388b8cd9c12d72ac289f12b96618f19c9e651c3
szczekulskij <szczekulskij@gmail.com>
2022-08-04 21:17:45 +0100

    add_backend

---
+++ backend/src/main/java/com/szczekulskij/monetmethis/MonetmethisApplication.java
@@ -0,0 +1,13 @@
+package com.szczekulskij.monetmethis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonetmethisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonetmethisApplication.class, args);
+	}
+
+}
