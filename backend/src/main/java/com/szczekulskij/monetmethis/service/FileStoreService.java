17c7e27d3e407f5df25a1126428f4b9616c31665
szczekulskij <szczekulskij@gmail.com>
2022-08-05 13:18:13 +0100

    add random photo functionality to backend

--- backend/src/main/java/com/szczekulskij/monetmethis/service/FileStoreService.java
+++ backend/src/main/java/com/szczekulskij/monetmethis/service/FileStoreService.java
@@ -16,7 +16,10 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.UUID;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 
@@ -30,8 +33,9 @@ public class FileStoreService {
         try {
 
             Path file;
-            if (isMonet){ file = monetPhotosLocation.resolve("0a5075d42a.jpg"); }
-            else { file = nonMonetPhotosLocation.resolve("0a0c3a6d07.jpg"); }
+            if (isMonet){ file = randomFileNameGenerator(monetPhotosLocation); }
+            else { file = randomFileNameGenerator(nonMonetPhotosLocation); }
+            // else { file = nonMonetPhotosLocation.resolve("0a0c3a6d07.jpg"); }
             Resource resource = new UrlResource(file.toUri());
 
             if (resource.exists() || resource.isReadable()) {
@@ -44,8 +48,38 @@ public class FileStoreService {
         }
     }
     
-    // public String randomFileNameGenerator(Path folder_location){
-    //     Path file = "0a5075d42a.jpg";
-    //     return Path file;
-    // }
+    public Path randomFileNameGenerator(Path folder_location){
+        // Get list of all the files
+        ArrayList<Path> allFiles = loadAllFiles(folder_location);
+
+        // Get random number from 0 to len(List) - 1
+        int listSize = allFiles.size();
+        Random rand = new Random();
+        int random = rand.nextInt(listSize);
+
+        // Get random filename
+        Path filename = allFiles.get(random);
+
+        System.out.println("allFiles" + allFiles);
+        System.out.println("filename" + filename);
+        
+        return folder_location.resolve(filename);
+    }
+
+    public ArrayList<Path> loadAllFiles(Path folder_location)  {
+        try {
+            return (ArrayList<Path>) Files.walk(folder_location, 1)
+                    .filter(path -> !path.equals(folder_location))
+                    .map(folder_location::relativize).collect(Collectors.toList());
+        }
+        catch (IOException e) {
+            try {
+                throw new Exception("Failed to read stored files", e);
+            } catch (Exception e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            }
+        }
+        return null;
+    }
 }
