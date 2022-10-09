87bdfebde07b9c527a4a7e2ced40f4f9e093babf
szczekulskij <szczekulskij@gmail.com>
2022-10-09 18:50:24 +0200

    save folders structure

---
+++ GAN/script.py
@@ -0,0 +1,16 @@
+# Make it so that you can run it via a shell command and it generates what you want it to aye
+def transformPhotoToMonet(input_photo):
+    '''
+    input_photo: either str - as in location of the photo
+    '''
+    
+    
+
+
+
+def main_func():
+    pass
+
+
+if __name__ == "__main__":
+    main_func()
