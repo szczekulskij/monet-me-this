7db235256ac8ca05ab940660af9d5625452a2555
szczekulskij <szczekulskij@gmail.com>
2022-10-30 19:37:18 +0000

    add 2 images

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -13,20 +13,28 @@ export default class Navigation2 extends Component {
       const { activeItem } = this.state
   
       return (
+        <>
         <Menu compact
-        style={{fontSize:"20px", width:"40%", textAlign: "center", marginTop: "50px", marginLeft: "30%", paddingLeft: "0px"}}>
-          <Menu.Item header 
-                name="Start"
-                onClick={this.handleItemClick}
-                style = {{fontSize:"25px", width:"75%", textAlign: "center", display: "inline-block"}}
-          />
-          <Menu.Item
-                name="Tutorial"
-                active={activeItem === "Monet's history"}
-                onClick={this.handleItemClick}
-                style = {{width:"25%", textAlign: "center", display: "inline-block", paddingTop: "25px"}}
-          />
-        </Menu>
+              style={{ fontSize: "20px", width: "60%", textAlign: "center", marginTop: "50px", marginLeft: "20%", paddingLeft: "0px" }}>
+              <Menu.Item header
+                  name="Start"
+                  onClick={this.handleItemClick}
+                  style={{ fontSize: "25px", width: "75%", textAlign: "center", display: "inline-block" }} />
+              <Menu.Item
+                  name="Tutorial"
+                  active={activeItem === "Monet's history"}
+                  onClick={this.handleItemClick}
+                  style={{ width: "25%", textAlign: "center", display: "inline-block", paddingTop: "25px" }} />
+          </Menu>
+          
+          <div style = {{display: "flex", overflow: "auto"}}>
+            {/* <h2>Hello</h2> */}
+            <img src="img/about.jpg" style={{display: "block", width: "29.85%",  marginLeft: "auto", }}/> 
+            <div style = {{borderLeft: "2px solid black", height: "100%", borderRight: "2px solid black"}}/>
+            <img src="img/about.jpg" style={{display: "block", width: "29.85%", marginRight: "auto"}}/> 
+          </div>
+          </>
+
       )
     }
   }
