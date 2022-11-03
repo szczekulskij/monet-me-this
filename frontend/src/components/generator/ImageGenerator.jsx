7be7c837a6d5e5840854798e4ed0e40c69de46fc
szczekulskij <szczekulskij@gmail.com>
2022-11-03 20:13:03 +0000

    add cors in flask & (not-working) monet gen backend call in frontend

--- frontend/src/components/generator/ImageGenerator.jsx
+++ frontend/src/components/generator/ImageGenerator.jsx
@@ -1,17 +1,108 @@
 import React, { Component , useState, useEffect} from 'react'
 import { Button, Image, Modal } from 'semantic-ui-react'
+import axios from 'axios';
+var FormData = require('form-data');
 
-function UploadModal() {
+class ImageGenerator extends Component {
+
+  state = {
+    beforeImage : "../img/before.jpg",
+    afterImage : "../img/after.jpg",
+    file : 0,
+    base64String : ""
+  }
+
+  setFile = (fil) =>
+  this.setState({file : fil})
+
+  setBeforeImage = (img) => {
+    this.setState({beforeImage : img})
+  }
+
+//   _imageEncode = (arrayBuffer) => {
+//     let u8 = new Uint8Array(arrayBuffer)
+//     let b64encoded = btoa([].reduce.call(new Uint8Array(arrayBuffer),function(p,c){return p+String.fromCharCode(c)},''))
+//     let mimetype="image/jpeg"
+//     return "data:"+mimetype+";base64,"+b64encoded
+// }
+
+
+  generateAndSetAfterImage = async (imageURL) => {
+    const backend_url = "http://127.0.0.1:5000/generate/image/monet" 
+    const data = new FormData();
+    data.append('image', imageURL);
+    // const res = await axios.post(backend_url, {'image' : this.state.file}, { headers: {'Content-Type': 'multipart/form-data'}})
+    const res = await axios.post(
+      backend_url, 
+      {'image' : this.state.file}, 
+      { headers: 
+        // {'responseType': 'arraybuffer',
+        {responseType: "bufferarray",
+        'Content-Type': 'multipart/form-data'}
+      }
+    )
+    
+    const buffer = Buffer.from(res.data, 'base64');
+    const image = this._imageEncode(res.data)
+
+    console.log(res.data);
+    console.log(image);
+    console.log(buffer);
+
+    const base64String = btoa(String.fromCharCode(...new Uint8Array(buffer)));
+    console.log("base64String: ",base64String)
+    this.setState({base64String : base64String})
+
+
+
+    // const newImageURL = await URL.createObjectURL(this._imageEncode(res.data))
+    // this.setState({afterImage : newImageURL})
+  }
+
+
+
+
+
+
+
+
+  render() {
+    return ( 
+    <div>
+      <h1 style = {{ width: "100%", textAlign: "center", paddingTop: "20px", height: "80px"}}> Monet's Image Generator </h1>
+      <div style = {{margin: "auto", width: "40%"}}>
+          <img src={this.state.beforeImage} style = {{width: "40%"}}/>
+          <img src="../img/arrow.png" style = {{width: "20%", paddingBottom : "10%"}}/>
+          {/* <img src={this.state.afterImage} style = {{width: "40%"}}/> */}
+          <img src={`data:image/jpg;base64,${this.state.base64String}`} alt="" style = {{width: "40%"}}/>
+          {/* <img src={`data:image/png;base64,${this.state.base64String}`} alt=""/> */}
+      </div>
+
+      <div style = {{margin: "auto", width: "30%", paddingTop: "30px"}}>
+        <div class="ui input" style = {{margin: "auto", padding: "0 0 0 0", width: "100%"}}>
+          <UploadModal 
+          setFile = {this.setFile}
+          setBeforeImage = {this.setBeforeImage}
+          generateAndSetAfterImage = {this.generateAndSetAfterImage}
+          />
+        </div>
+      </div>
+    </div>
+     );
+  }
+}
+
+
+
+// -----------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------
+
+function UploadModal(props) {
   const [open, setOpen] = useState(false)
   // const [image, setImages] = useState()
   const [imageURL, setimageURL] = useState("https://react.semantic-ui.com/images/wireframe/image-square.png")
-
-  // useEffect(() =>{
-  //   let newImageURL = URL.createObjectURL(image)
-  //   setimageURL(newImageURL)
-  // }, [image])
-
-
 
 
   function onImageChange(e) {
@@ -19,7 +110,7 @@ function UploadModal() {
     let newImageURL = URL.createObjectURL(file)
     setimageURL(newImageURL)
     console.log("imageURL:", imageURL)
-
+    props.setFile(file)
   }
 
   return (
@@ -31,16 +122,19 @@ function UploadModal() {
     >
       <Modal.Header>Upload image</Modal.Header>
       <Modal.Content image>
-      {/* <input type="file" multiple accept="image/*" onChange={setOpen(false)} /> */}
         <Image size='medium' src={imageURL} wrapped />
         <Modal.Description>
-        <input type="file" accept="image/*" onChange={onImageChange}/>
+        <input name="image" type="file" accept="image/*" onChange={onImageChange}/>
           <p>Would you like to upload this image?</p>
         </Modal.Description>
       </Modal.Content>
       <Modal.Actions>
         <Button onClick={() => setOpen(false)}>Cancel</Button>
-        <Button onClick={() => setOpen(false)} positive>
+        <Button positive onClick={() => {
+          setOpen(false); 
+          props.setBeforeImage(imageURL);
+          props.generateAndSetAfterImage(imageURL);
+          }}>
           Ok
         </Button>
       </Modal.Actions>
@@ -49,29 +143,4 @@ function UploadModal() {
 }
 
 
-
-
-class ImageGenerator extends Component {
-  render() {
-    return ( 
-    <div>
-      <h1 style = {{ width: "100%", textAlign: "center", paddingTop: "20px", height: "80px"}}> Monet's Image Generator </h1>
-      <div style = {{margin: "auto", width: "40%"}}>
-          <img src="../img/before.jpg" style = {{width: "40%"}}/>
-          <img src="../img/arrow.png" style = {{width: "20%", paddingBottom : "10%"}}/>
-          <img src="../img/after.jpg" style = {{width: "40%"}}/>
-      </div>
-
-      <div style = {{margin: "auto", width: "30%", paddingTop: "30px"}}>
-        <div class="ui input" style = {{margin: "auto", padding: "0 0 0 0", width: "100%"}}>
-          {/* <input type="file" placeholder="Search..."/>
-          <button class="ui button">Upload</button> */}
-          <UploadModal></UploadModal>
-        </div>
-      </div>
-    </div>
-     );
-  }
-}
- 
 export default ImageGenerator;
