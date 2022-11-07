58ca970abaa136ca7c46c239f7fe5f22b8a57c4c
szczekulskij <szczekulskij@gmail.com>
2022-11-07 12:16:58 +0000

    add cross origin fix

--- backend/src/main/java/com/szczekulskij/monetmethis/controller/ImageUploadController.java
+++ backend/src/main/java/com/szczekulskij/monetmethis/controller/ImageUploadController.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.io.ObjectInputFilter.Config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -27,22 +28,27 @@ public class ImageUploadController {
     @Autowired
     FileLoadingService fileLoadingService;
 
+
+    @CrossOrigin
     @GetMapping(value = "/image", produces = MediaType.IMAGE_JPEG_VALUE)
     public ResponseEntity<Resource> randomNonMonet(){
         Resource file = fileLoadingService.loadImage("none", false);
         return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"").body(file);
     }
+    @CrossOrigin
     @GetMapping(value = "/monet/original", produces = MediaType.IMAGE_JPEG_VALUE)
     public ResponseEntity<Resource> randomMonet(){
         Resource file = fileLoadingService.loadImage("monet", true);
         return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"").body(file);
     }
 
+    @CrossOrigin
     @GetMapping(value = "/monet/generated", produces = MediaType.IMAGE_JPEG_VALUE)
     public ResponseEntity<Resource> randomMonetsque(){
         Resource file = fileLoadingService.loadImage("monet", false);
         return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"").body(file);
     }
+    @CrossOrigin
     @GetMapping(value = "/generated", produces = MediaType.IMAGE_JPEG_VALUE)
     public ResponseEntity<Resource> getGeneratedImage(){
         Resource file = fileLoadingService.loadImage("generated", false);
