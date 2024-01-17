394578688bb121d419d697d52df9c9b545c4c9c5
szczekulskij <szczekulskij@gmail.com>
2024-01-17 07:29:24 -0800

    pretty much finish the UI part of the project

--- new_frontend/src/components/mainGame/game.js
+++ new_frontend/src/components/mainGame/game.js
@@ -21,7 +21,7 @@ class Game extends React.Component {
   };
 
   handleStartClick = () => {
-    this.setState({ showGuessOptions: true });
+    this.setState({ showButtons: false, showGuessOptions: true });
   };
   
   handleGuessOptionClick = (guesses) => {
@@ -52,6 +52,7 @@ class Game extends React.Component {
           justifyContent: 'center',
           minHeight: '100vh',
           gap: '20px',
+          marginTop: '30px', // Decrease the top margin to shift everything up
         }}
       >
         <Box
@@ -59,7 +60,7 @@ class Game extends React.Component {
             display: 'flex',
             gap: '20px',
             filter: showButtons ? 'brightness(50%)' : 'none',
-            width: '80%',
+            width: '80%', // Increase the width to make the images bigger
           }}
         >
           <div onClick={this.handleImageClick}>
@@ -77,31 +78,32 @@ class Game extends React.Component {
             />
           </div>
         </Box>
-        {!showButtons && (
-          <Box style={{ 
-            width: '80%', 
-            height: '2px', 
-            backgroundColor: 'grey', 
-            position: 'relative',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}>
+        <Box style={{ 
+          width: '80%', // Increase the width to make the horizontal line bigger
+          height: '6px', // Increase the height to make the line thicker
+          backgroundColor: 'grey', 
+          position: 'relative',
+          display: 'flex',
+          alignItems: 'center',
+          marginTop: '20px', // Reserve space for the horizontal line
+        }}>
+          {!showButtons && (
             <div style={{ 
               width: `${scoreRatio * 100}%`, 
-              height: '2px', 
+              height: '6px', // Increase the height to make the line thicker
               backgroundColor: scoreColor, 
               transition: 'width 2s ease' 
             }} />
-            <Typography style={{ 
-              position: 'absolute', 
-              color: 'white', 
-              fontSize: '12px',
-            }}>
-              {score}/{totalGuesses}
-            </Typography>
-          </Box>
-        )}
+          )}
+          <Typography style={{ 
+            position: 'absolute', 
+            color: 'white', 
+            fontSize: '12px',
+            right: '0', // Position the number on the right
+          }}>
+            {score}/{totalGuesses}
+          </Typography>
+        </Box>
         {showButtons && (
           <Box
             style={{
@@ -122,14 +124,62 @@ class Game extends React.Component {
             </Button>
           </Box>
         )}
-        <Dialog open={showGuessOptions} onClose={this.handleClose}>
-          <DialogTitle>Choose the number of guesses</DialogTitle>
-          <DialogContent>
-            <Button onClick={() => this.handleGuessOptionClick(10)}>10 guesses</Button>
-            <Button onClick={() => this.handleGuessOptionClick(25)}>25 guesses</Button>
-            <Button onClick={() => this.handleGuessOptionClick(50)}>50 guesses</Button>
-          </DialogContent>
-        </Dialog>
+        {showGuessOptions && (
+          <Box
+            style={{
+              position: 'absolute',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'center',
+              gap: '20px',
+              backgroundColor: '#f5f5f5', // Change the background color of the popup
+              border: '2px solid #333', // Add a border to the popup
+              borderRadius: '10px', // Add rounded corners to the popup
+              padding: '20px', // Add some padding to the popup
+            }}
+          >
+            <Typography style={{ fontSize: '20px', fontWeight: 'bold' }}>
+              Choose the number of guesses
+            </Typography>
+            <Button 
+              variant="contained" 
+              onClick={() => this.handleGuessOptionClick(10)}
+              style={{
+                backgroundColor: '#4caf50', // Change the background color of the button
+                '&:hover': {
+                  backgroundColor: '#388e3c', // Change the hover color of the button
+                },
+              }}
+            >
+              10 guesses
+            </Button>
+            <Button 
+              variant="contained" 
+              onClick={() => this.handleGuessOptionClick(25)}
+              style={{
+                backgroundColor: '#ff9800', // Change the background color of the button
+                '&:hover': {
+                  backgroundColor: '#f57c00', // Change the hover color of the button
+                },
+              }}
+            >
+              25 guesses
+            </Button>
+            <Button 
+              variant="contained" 
+              onClick={() => this.handleGuessOptionClick(50)}
+              style={{
+                backgroundColor: '#f44336', // Change the background color of the button
+                '&:hover': {
+                  backgroundColor: '#d32f2f', // Change the hover color of the button
+                },
+              }}
+            >
+              50 guesses
+            </Button>
+          </Box>
+        )}
         <Dialog open={showMessage} onClose={this.handleClose}>
           <DialogTitle>{"Tutorial"}</DialogTitle>
           <DialogContent>
